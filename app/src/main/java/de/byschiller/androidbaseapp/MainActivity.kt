109718d7c276ae1293b2f187a0169c6496a0083b
ArnoSchiller <arno.schiller@student.jade-hs.de>
2022-08-03 14:19:41 +0200

    Created empty project Empty Compose Activity (Material3)

---
+++ app/src/main/java/de/byschiller/androidbaseapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package de.byschiller.androidbaseapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import de.byschiller.androidbaseapp.ui.theme.AndroidBaseAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidBaseAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("AndroidBaseApp")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AndroidBaseAppTheme {
+        Greeting("Android")
+    }
+}
